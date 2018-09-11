181a5a387b5d83b50eb5c9e269bccfa7d30ff8ea
Dinesh Ram Kumar <tdineshramkumar@gmail.com>
2018-09-11 16:46:28 +0530

    search wiki via command line

---
+++ wiki_search.py
@@ -0,0 +1,103 @@
+"""
+T Dinesh Ram Kumar
+Shows extracts from Wiki using Wikipedia API
+Based on:
+1.  https://www.mediawiki.org/wiki/User:SSethi_(WMF)/Sandbox/API:Opensearch
+2.  https://en.wikipedia.org/api/rest_v1/#!/Page_content/get_page_summary_title
+"""
+
+import requests
+import sys
+from getopt import GetoptError, getopt
+from os.path import basename
+# comment the selenium usage if browser support not needed 
+from selenium import webdriver
+from os.path import exists
+
+# Refer to Wikipedia API page for explanations
+DEFAULT_LIMIT = 10
+DEFAULT_NAMESPACE = 0
+URL = "https://en.wikipedia.org/w/api.php"
+SUMMARY_URL = "https://en.wikipedia.org/api/rest_v1/page/summary/{}?redirect=true"
+SHOW_IN_BROWSER = False
+DETAILED = False
+PARSABLE = False
+
+
+def usage():
+    print("Usage: {} [OPTION...] command".format(basename(sys.argv[0])))
+    print("Searches and gets extracts from the wiki using Wikipedia API in OpenSearch Format.")
+    print("Options:")
+    print("\t--help, -h\n\t\tGive the usage.")
+    print("\t--browser, -b\n\t\tOpen in browser.")
+    print("\t--parse, -p\n\t\tPrint in parse-able format (OpenSearch Format).")
+    print("\t--detail, -d\n\t\tFetch and display a more detailed Extract.")
+    print("\t--limit, -l\n\t\tSet the limit. Default limit = {!s}".format(DEFAULT_LIMIT))
+    print("\t--namespace, -n\n\t\tSet the wikipedia namespace. Default namespace = {!s}".format(DEFAULT_NAMESPACE))
+
+
+try:
+    SHORT_OPTIONS = "hdl:n:bp"
+    LONG_OPTIONS = ["help", "detail", "limit=", "namespace=", "browser", "parse"]
+    opts, argv = getopt(args=sys.argv[1:], shortopts=SHORT_OPTIONS, longopts=LONG_OPTIONS)
+    SEARCH_TERM = " ".join(argv).strip()
+    if SEARCH_TERM == '':
+        print("Specify the search term.")
+        usage()
+        sys.exit()
+except GetoptError as error:    # if invalid options then print error, usage and then exit
+    print(error)
+    usage()
+    sys.exit(-1)
+
+for o, a in opts:
+    if o in ("-h", "--help"):   # used to show usage
+        usage()
+        sys.exit(0)
+    if o in ("--browser", "-b"):
+        SHOW_IN_BROWSER = True
+    if o in ("--parse", "-p"):
+        PARSABLE = True
+    if o in ("--detail", "-d"):
+        DETAILED = True
+    if o in ("--limit", "-l"):
+        DEFAULT_LIMIT = int(a)
+    if o in ("--namespace", "-n"):
+        DEFAULT_NAMESPACE = int(a)
+
+PARAMS = {
+    'action': "opensearch",
+    'search': SEARCH_TERM,
+    'limit': DEFAULT_LIMIT,
+    'namespace': DEFAULT_NAMESPACE,
+    'format': "json"
+}
+
+detailed_wiki = lambda title: requests.get(SUMMARY_URL.format(title)).json()['extract']
+
+R = requests.get(url=URL, params=PARAMS)    # Send the GET request with desired parameters
+DATA = R.json()                             # convert it to JSON (OpenSearch Specifications)
+
+if PARSABLE and DETAILED:   # If print in detailed and parsable manner
+    for index, title in enumerate(DATA[1]):
+        DATA[2][index] = detailed_wiki(title)
+    print(DATA)
+elif PARSABLE:  # if only parsable manner
+    print(DATA)
+else:
+    # Print the contents
+    if DATA[1]:     # if some search results then ..
+        for index, (title, data, url) in enumerate(zip(DATA[1], DATA[2], DATA[3]), 1):
+            if DETAILED:
+                data = detailed_wiki(title)
+            print("{0!s}. \033[031;1m{1}\033[0m:\t[{2}]\n\t\033[1m{3}\033[0m\n".format(index, title, url, data))
+
+        if SHOW_IN_BROWSER:
+            print("Browser View: ")
+            index = int(input("Enter index [{}-{}]:".format(1, len(DATA[1]))))
+            index = max(min(len(DATA[1]), index), 1) - 1
+            driver = webdriver.Firefox()
+            driver.get(DATA[3][index])
+    else:
+        print("No Results found for {}.".format(DATA[0]))
+
